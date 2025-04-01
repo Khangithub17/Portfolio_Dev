4bffec555c3e34687fe5db22eea8dea296fd6095
Shahbaz Khan <121373547+Khangithub17@users.noreply.github.com>
2025-04-01 10:35:20 +0530

    fix: update dependencies and PostCSS configuration

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Frontend Developer | MERN Stack Developer",
   description: "My name is Shahbaz Khan. I am a passionate and enthusiastic Frontend Developer with expertise in React.js, JavaScript, and UI/UX design. I love to explore new technologies and solve problems through coding. With 1.5 years of experience, I have worked on multiple client projects, building high-performance web applications using React.js, Next.js, and No-Code platforms like Webflow, Wix, Framer, and Shopify. Currently, I am also expanding my knowledge in Data Analytics. I am always eager to take on new challenges and contribute to impactful projects.",
   email: "codewithkhan17@gmail.com",
-  phone: "+918758268704",
+  phone: "+919998111714",
   address: "Gujarat, India",
   github: "https://github.com/Khangithub17",
   linkedIn: "https://www.linkedin.com/in/shahbazkhan17/",
